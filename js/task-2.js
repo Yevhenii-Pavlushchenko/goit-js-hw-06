b6aeac43b4986eeee1c3883edce1e6b6915ffef7
JustFun <paw93@ukr.net>
2026-01-27 15:03:35 +0200

    finish second task

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,21 @@
+class Storage {
+  //?Оголошення приватних змiнних
+  #items;
+
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    return this.items.push(item);
+  }
+  removeItem(item) {
+    return (this.items = this.items.filter(product => product !== item));
+  }
+}
+
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
